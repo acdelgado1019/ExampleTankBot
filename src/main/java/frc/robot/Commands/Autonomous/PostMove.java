2b1ff49e91c88ac6a23b5b2b22e788e2ac081a13
Antonio Delgado <acdelgado1019@gmail.com>
2022-08-01 22:15:21 -0500

    Haptics on Auto

--- src/main/java/frc/robot/Commands/Autonomous/PostMove.java
+++ src/main/java/frc/robot/Commands/Autonomous/PostMove.java
@@ -12,15 +12,20 @@ public class PostMove extends SequentialCommandGroup {
             Timer.delay(5);
             SmartDashboard.putString("Auto Step", "Run Intake");
             AutoCommand.runIntake(-Constants.HORIZONTAL_INTAKE_SPEED);
+            Timer.delay(1);
             SmartDashboard.putString("Auto Step", "Rotate");
             AutoCommand.rotate(1);
         } else if (mode == "Two Ball Auto"){
             Timer.delay(5);
+            SmartDashboard.putString("Auto Step", "Stop Intake");
             AutoCommand.runIntake(0);
+            SmartDashboard.putString("Auto Step", "Shooting");
             AutoCommand.limelightShoot(Constants.SHOOTER_LOW_SPEED);
         } else if (mode == "Three Ball Auto") {
             Timer.delay(5);
+            SmartDashboard.putString("Auto Step", "Stop Intake");
             AutoCommand.runIntake(0);
+            SmartDashboard.putString("Auto Step", "Shooting");
             AutoCommand.limelightShoot(Constants.SHOOTER_LOW_SPEED);
         }
     }
--- src/main/java/frc/robot/Commands/Autonomous/PreMove.java
+++ src/main/java/frc/robot/Commands/Autonomous/PreMove.java
@@ -9,22 +9,27 @@ public class PreMove extends SequentialCommandGroup {
     
     public PreMove(String mode){
         if (mode == "One Ball Auto"){
-            AutoCommand.timerDrive(0.6, 2);
             SmartDashboard.putString("Auto Step", "Delay");
             Timer.delay(0);
-
             SmartDashboard.putString("Auto Step", "Intake Down");
             AutoCommand.lowerIntake();
             SmartDashboard.putString("Auto Step", "Shooting");
             AutoCommand.limelightShoot(Constants.SHOOTER_HI_SPEED);
             SmartDashboard.putString("Auto Step", "Run Away");
         } else if (mode == "Two Ball Auto"){
+            SmartDashboard.putString("Auto Step", "Intake Down");
             AutoCommand.lowerIntake();
+            SmartDashboard.putString("Auto Step", "Run Intake");
             AutoCommand.runIntake(Constants.HORIZONTAL_INTAKE_SPEED);
+            SmartDashboard.putString("Auto Step", "Collecting");
         } else if (mode == "Three Ball Auto") {
+            SmartDashboard.putString("Auto Step", "Shooting");
             AutoCommand.limelightShoot(Constants.SHOOTER_HI_SPEED);
+            SmartDashboard.putString("Auto Step", "Intake Down");
             AutoCommand.lowerIntake();
+            SmartDashboard.putString("Auto Step", "Run Intake");
             AutoCommand.runIntake(Constants.HORIZONTAL_INTAKE_SPEED);
+            SmartDashboard.putString("Auto Step", "Collecting");
         }
     }
 }
